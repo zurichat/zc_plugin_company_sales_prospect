c5ed0ff068c045fd74006c998f5bb54283a5965b
hengage <tunecarterkillz@gmail.com>
2021-09-02 00:05:51 +0100

    created 'info' endpoint

--- prospectapp/urls.py
+++ prospectapp/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+
 from .sidebar.views import *
+from .info import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",TemplateView.as_view(template_name='index.html')),
-    path("sidebar/", sidebar)
+    path("sidebar/", sidebar),
+    path('api/info', views.info)
 ]
