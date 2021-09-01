37a8f36b2e8441402ee35a4445475036f2d0dd0e
Abdulrahman Mohammed <abdmuhammad610@gmail.com>
2021-09-01 16:16:42 +0000

    stea

--- prospect/models.py
+++ prospect/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Prospect(models.Model):
+    """Model definition for Prospect."""
+   
+    # TODO: Define fields here
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    title = models.CharField(max_length=50)
+    company = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=255, default='NA')
+    handler= models.CharField(max_length=200,default="Rosita")
+    updated = models.DateTimeField(auto_now=True)
+
+
+    # uncomment one the deal model is done
+    # deal_status = models.ForeignKey(Deal)
+
+    class Meta:
+        """Meta definition for Prospect."""
+
+        verbose_name = 'Prospect'
+        verbose_name_plural = 'Prospects'
+
+    def __str__(self):
+        """Unicode representation of Prospect."""
+        pass
+
+
+
+    
