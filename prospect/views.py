7649a94250bb6f405c5671f983cd8e21ad556087
noornee <kendrickbon11@gmail.com>
2021-09-01 13:52:56 +0100

    welcome view

--- prospect/views.py
+++ prospect/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.conf import settings
+from django.core.mail import send_mail
+from django.http import JsonResponse
 # Create your views here.
+
+def welcome(request):
+    """
+    this functions sends a welcome email to new leads
+    """
+    send_mail(
+        subject = f'Welcome {request.user}',
+        message = f'Hello {request.user} your account was successfully created',
+        from_email = settings.EMAIL_HOST_USER,
+        recipient_list = ['test1@dummy.com']
+            )
+    return JsonResponse({"message":"welcome mail has been sent successfully"})
+
+
