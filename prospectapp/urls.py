d5513170ffdea47282179165ecb9084dd103408e
noornee <kendrickbon11@gmail.com>
2021-09-01 13:53:58 +0100

    included the url pattern for the prospect

--- prospectapp/urls.py
+++ prospectapp/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 from .sidebar.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",TemplateView.as_view(template_name='index.html')),
-    path("sidebar/", sidebar)
+    path("sidebar/", sidebar), 
+    path('',include('prospect.urls'))
 ]
