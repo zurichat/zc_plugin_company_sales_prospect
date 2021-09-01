df2d05785b596e9b912e76643eaa7a1cefa15d4f
Patrick Idungafa <patrickidungafa2@gmail.com>
2021-09-01 13:38:56 +0100

    Defined the model for prospects

--- prospect/admin.py
+++ prospect/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ProspectDetails
 
 # Register your models here.
+admin.site.register(ProspectDetails)
--- prospect/models.py
+++ prospect/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ProspectDetails(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150, null=True)
+    phone = models.CharField(max_length=15, null=True)
+    email = models.EmailField(max_length=200, null=True)
+    profile_pic = models.ImageField(null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.profile_pic + str(self.name)
+
