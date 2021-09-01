076369da6d3366bfd697f89031f4ac389d9de6a2
Maureen Anyanwu <maureenanyanwu519@gmail.com>
2021-09-01 19:50:44 +0100

    exported icons from figma to reusable svgs

--- frontend/epictetus/src/App.js
+++ frontend/epictetus/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Test from './components/Test';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
-      <h1 className="text-4xl font-bold">Company Sales Prospect</h1>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/test" component={Test} />    
+        </Switch>
+      </div>
+  </Router>
   );
 }
 
---
+++ frontend/epictetus/src/components/Test.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ChevronDown from './svg/ChevronDown'
+import ChevronLeft from './svg/ChevronLeft'
+import ChevronRight from './svg/ChevronRight'
+import ChevronUp from './svg/ChevronUp'
+import DotIcon from './svg/DotIcon'
+import FilterIcon from './svg/FilterIcon'
+import SearchIcon from './svg/SearchIcon'
+import SettingsIcon from './svg/SettingsIcon'
+
+export default function Test() {
+    return (
+       <>
+           <h2  className="text-4xl font-bold pb-10 m-10" >Svg Icons exported from figma</h2> 
+           <div class="flex justify-around ...">
+                <ChevronDown/>
+                <ChevronUp/>
+                <ChevronRight/>
+                <ChevronLeft/>
+                <DotIcon color="#7F7A00"/>
+                <FilterIcon/>
+                <SearchIcon/>
+                <SettingsIcon/> 
+            </div>
+        </>
+    )
+}
