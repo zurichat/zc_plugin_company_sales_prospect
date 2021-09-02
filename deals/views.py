e27fba40b689e3a13dd9c788dc8b4278f917e82e
Peter <pebueku@gmail.com>
2021-09-02 16:57:31 +0100

    Added DealUpdateView to deals/view.py

--- deals/views.py
+++ deals/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.conf import settings
 
+# Added generic view file and imported models
+from django.views import generic
+from .models import Deal
+
+
 PLUGIN_ID = settings.PLUGIN_ID
 ORGANISATION_ID = settings.ORGANISATION_ID
 PLUGIN_NAME = settings.PLUGIN_NAME
+
+# Added UpdateView class
+class DealUpdateView(generic.UpdateView):
+    template_name = 'update.html'
+    model = Deal
+    fields = ()
