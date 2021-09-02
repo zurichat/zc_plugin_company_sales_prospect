0ed5ee44ff0365004a53aa68454b4efa83c905f7
Tropixel <ajibadeabd@gmail.com>
2021-09-02 14:56:20 +0100

    pulled latest

--- deals/serializers.py
+++ deals/serializers.py
@@ -3,4 +3,6 @@ from rest_framework import serializers
 class DealSerializer(serializers.Serializer):
     _id = serializers.CharField(max_length=100)
     prospect_id= serializers.CharField(max_length=100)
-    Status= serializers.CharField(max_length=100)
+    status= serializers.CharField(max_length=100)
+    title= serializers.CharField(max_length=100)
+    amount= serializers.CharField(max_length=100)
