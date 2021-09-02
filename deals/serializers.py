fd7860fea0a37a000ba3db2de91089f18453edec
Tropixel <ajibadeabd@gmail.com>
2021-09-02 12:16:13 +0100

    created deal serielizer

---
+++ deals/serializers.py
@@ -0,0 +1,4 @@
+class DealSerializer(serializers.Serializer):
+    _id = serializers.CharField(max_length=100)
+    Prospect_id= serializers.CharField(max_length=100)
+    Status= serializers.CharField(max_length=100)
