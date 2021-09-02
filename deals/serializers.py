01f2ed9cafa598c0294d3586ef1f61b70dd2bef4
Tropixel <ajibadeabd@gmail.com>
2021-09-02 12:47:01 +0100

    updated my code

--- deals/serializers.py
+++ deals/serializers.py
@@ -1,3 +1,5 @@
+from rest_framework import serializers
+
 class DealSerializer(serializers.Serializer):
     _id = serializers.CharField(max_length=100)
     Prospect_id= serializers.CharField(max_length=100)
