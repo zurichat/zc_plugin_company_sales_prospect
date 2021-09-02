70eac53f4e9129a948d2b0ad1ea4dd6969626d77
sparkle666 <45319713+sparkle666@users.noreply.github.com>
2021-09-02 17:44:22 +0100

    Revert "Fixed views not found"

--- deals/views.py
+++ deals/views.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render
-from django.conf import settings
 
-PLUGIN_ID = settings.PLUGIN_ID
-ORGANISATION_ID = settings.ORGANISATION_ID
-PLUGIN_NAME = settings.PLUGIN_NAME
+# Create your views here.
