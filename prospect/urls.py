ae4426b7e6b529b7bbb2a9e9951850fda0b998b9
temitayopelumi <olalekantemitayo7@gmail.com>
2021-09-02 13:09:08 +0100

    search endpoint

---
+++ prospect/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('search/<str:search>', views.SearchProspects, name='s'),
+]
--- prospect/views.py
+++ prospect/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import filters
+from rest_framework import generics
 
-# Create your views here.
+from django.http import JsonResponse
+
+def SearchProspects(request, search):
+    import requests
+    url = "https://zccore.herokuapp.com/data/read/000000000000000000000000/prospects/612a3a914acf115e685df8e3/"
+    response = requests.request("GET", url)
+    r = response.json()
+    print(response.status_code)
+    if response.status_code == 200:    
+        liste=[]
+        for i  in r['data']:
+            if (search in i['first_name']) or (search in i['last_name']) or (search in i['email']) or  (search in i['company']):
+                liste.append(i)
+        return JsonResponse(liste,safe=False)
