d2611bd3ae45a49b26613ea9306021a1970cd831
sparkle666 <sikky606@gmail.com>
2021-09-02 23:31:02 +0100

    fixed double prospect urls

--- prospect/urls.py
+++ prospect/urls.py
@@ -5,7 +5,7 @@ from .views import ProspectsDetailView, ProspectsUpdateView, SearchProspects,Pro
 urlpatterns = [
     path('search/<str:search>', SearchProspects, name='search'),
     path('create', ProspectsCreateView.as_view()),
-    path('prospects/', ProspectsListView.as_view(), name="prospects"),
+    path('', ProspectsListView.as_view(), name="prospects"),
     path('<str:id>/', ProspectsDetailView.as_view(), name="detail prospects"),
     path('update/', ProspectsUpdateView.as_view()),
 ] 
