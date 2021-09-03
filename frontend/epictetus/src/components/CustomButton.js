20d1d0933685ef5ade596a4945addce7b04b9c31
EmmanuelObi <63916312+EmmanuelObi@users.noreply.github.com>
2021-09-03 01:58:55 +0100

    completed issue #41 general custom button component

---
+++ frontend/epictetus/src/components/CustomButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CustomButton = (props) => {
+
+    return (
+        <>
+            <button className="btn text-primary border-primary">{props.value}</button>
+        </>
+    )
+}
+
+export default CustomButton
