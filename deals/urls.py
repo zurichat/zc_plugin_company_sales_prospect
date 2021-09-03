d46391870a31439f9690c94dc87e8f66f2ecdc89
Peter Ebueku <pebueku@gmail.com>
2021-09-03 16:31:48 +0100

    Added a backslash

--- deals/urls.py
+++ deals/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
   path("create/", DealCreateView.as_view()),
   path("update/<str:id>/", DealUpdateView.as_view()),
   path('list/', DealsListView.as_view()),
-  path('<str:id>', DealsDetailView.as_view()),
+  path('<str:id>/', DealsDetailView.as_view()),
 ]
