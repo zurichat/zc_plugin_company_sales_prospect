e43d941b029ebe21d56442d153acf897c2829f46
Peter Ebueku <pebueku@gmail.com>
2021-09-03 16:24:16 +0100

    Added DetailView

--- deals/urls.py
+++ deals/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from deals.views import DealCreateView, DealUpdateView, DealsListView
+from deals.views import DealCreateView, DealUpdateView, DealsListView, DealsDetailView
 
 urlpatterns = [
   path("create/", DealCreateView.as_view()),
   path("update/<str:id>/", DealUpdateView.as_view()),
-  path('list/', DealsListView.as_view())
+  path('list/', DealsListView.as_view()),
+  path('<str:id>', DealsDetailView.as_view()),
 ]
