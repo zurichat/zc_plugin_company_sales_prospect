1ae7e5f6f76b5a74105ff271c2a216abb85bb86d
faboade oluwatosin <Faboadeoluwatosin@gmail.com>
2021-09-03 09:13:30 +0100

    edit sales prospect form

--- frontend/epictetus/src/App.js
+++ frontend/epictetus/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import EditProspectSales from './components/EditProspectSales';
 
 function App() {
   return (
-    <div className="app">
-      <h1>Company Sales Prospect</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/EditProspectSales" component={EditProspectSales} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
