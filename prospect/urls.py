4bb4bc27f068a4d268c16ed4ce4e3ec193e442ea
sparkle666 <sikky606@gmail.com>
2021-09-03 01:21:13 +0100

    created a deals create endpoint

--- prospect/urls.py
+++ prospect/urls.py
@@ -4,7 +4,7 @@ from .views import ProspectsDetailView, ProspectsUpdateView, SearchProspects,Pro
 
 urlpatterns = [
     path('search/<str:search>', SearchProspects, name='search'),
-    path('create', ProspectsCreateView.as_view()),
+    path('create/', ProspectsCreateView.as_view()),
     path('', ProspectsListView.as_view(), name="prospects"),
     path('<str:id>/', ProspectsDetailView.as_view(), name="detail prospects"),
     path('update/', ProspectsUpdateView.as_view()),
