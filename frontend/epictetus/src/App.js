d47fef9187a1ed31634c9a6b1a56f1a4fd76d69e
uzor13 <p.uzor99@gmail.com>
2021-09-03 13:21:12 +0100

    Styled the edit deals form

--- frontend/epictetus/src/App.js
+++ frontend/epictetus/src/App.js
@@ -3,27 +3,24 @@ import Test from './components/Test';
 import Home from './components/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CreateDealForm from './components/CreateDealForm';
+import EditProspectSales from './components/EditProspectSales';
 
 function App() {
-    return ( <
-        Router >
-        <
-        div className = "App" >
-        <
-        Switch >
-        <
-        Route exact path = "/"
-        component = { Home }
-        /> <
-        Route exact path = "/test"
-        component = { Test }
-        />  <
-        Route exact path = "/"
-        component = { CreateDealForm }
-        />  < /
-        Switch > <
-        /div> < /
-        Router >
+    return ( 
+    <Router>
+        <div className = "App">
+        <Switch>
+        <Route exact path = "/"
+        component = { Home }/> 
+        <Route exact path = "/test"
+        component = { Test }/>  
+        <Route exact path = "/"
+        component = { CreateDealForm }/>  
+        <Route exact path = "/prospect"
+        component = { EditProspectSales }/>  
+        </Switch> 
+        </div> 
+        </Router>
     );
 }
 
--- frontend/epictetus/tailwind.config.js
+++ frontend/epictetus/tailwind.config.js
@@ -1,42 +1,41 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-    colors:{
-      green: {
-        light: "#ACFFE6",
-        lighter: "#F0FFFA",
-        default: " #00B87C"
-      },
-      pink: {
-        light: "#FFD3D3"
-      },
-      white: {
-        default: "#FFFFFF"
-      },
-      yellow: {
-        default: "#FFFDCD"
-      },
-      blue: {
-        default: "#CDF3FF",
-        darker: "#00057F"
-    },
-    green: {
-      default: "#CDFFCD",
-      darker: "#007F00"
-    },
-    red: {
-      default: "#F40101"
-    },
-    black: {
-      default: "#393939"
-    }
-
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
-}
+	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+		colors: {
+			green: {
+				light: "#ACFFE6",
+				lighter: "#F0FFFA",
+				default: " #00B87C",
+			},
+			pink: {
+				light: "#FFD3D3",
+			},
+			white: {
+				default: "#FFFFFF",
+			},
+			yellow: {
+				default: "#FFFDCD",
+			},
+			blue: {
+				default: "#CDF3FF",
+				darker: "#00057F",
+			},
+			red: {
+				default: "#F40101",
+			},
+			black: {
+				default: "#393939",
+			},
+			gray: {
+				default: "#8B8B8B",
+				100: "#A1A1A1"
+			},
+		},
+		variants: {
+			extend: {},
+		},
+		plugins: [],
+	},
+};
