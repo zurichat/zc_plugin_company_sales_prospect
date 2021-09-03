209bd2e8512230d14fe6c26b50e84b3e7758be87
jamil <jamilujibrilla01@gmail.com>
2021-09-03 17:40:16 +0000

    uploading header

--- frontend/epictetus/src/App.js
+++ frontend/epictetus/src/App.js
@@ -1,27 +1,26 @@
-import './App.css';
-import Test from './components/Test';
-import Home from './components/Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import CreateDealForm from './components/CreateDealForm';
-import EditProspectSales from './components/EditProspectSales';
+import "./App.css";
+import Test from "./components/Test";
+import Home from "./components/Home";
+import Header from "./components/Header";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CreateDealForm from "./components/CreateDealForm";
+import EditProspectSales from "./components/EditProspectSales";
 
 function App() {
-    return ( 
+  return (
     <Router>
-        <div className = "App">
+      <div className="App">
+        <Header />
         <Switch>
-        <Route exact path = "/"
-        component = { Home }/> 
-        <Route exact path = "/test"
-        component = { Test }/>  
-        <Route exact path = "/"
-        component = { CreateDealForm }/>  
-        <Route exact path = "/prospect"
-        component = { EditProspectSales }/>  
-        </Switch> 
-        </div> 
-        </Router>
-    );
+          <Route exact path="/" component={Home} />
+          <Route exact path="/test" component={Test} />
+          <Route exact path="/" component={CreateDealForm} />
+          <Route exact path="/prospect" component={EditProspectSales} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- frontend/epictetus/tailwind.config.js
+++ frontend/epictetus/tailwind.config.js
@@ -1,70 +1,27 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-<<<<<<< HEAD
-	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {},
-		colors: {
-			green: {
-				light: "#ACFFE6",
-				lighter: "#F0FFFA",
-				default: " #00B87C",
-			},
-			pink: {
-				light: "#FFD3D3",
-			},
-			white: {
-				default: "#FFFFFF",
-			},
-			yellow: {
-				default: "#FFFDCD",
-			},
-			blue: {
-				default: "#CDF3FF",
-				darker: "#00057F",
-			},
-			red: {
-				default: "#F40101",
-			},
-			black: {
-				default: "#393939",
-			},
-			gray: {
-				default: "#8B8B8B",
-				100: "#A1A1A1"
-			},
-		},
-		variants: {
-			extend: {},
-		},
-		plugins: [],
-	},
-};
-=======
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
+      transparent: "transparent",
+      current: "currentColor",
       black: colors.black,
       white: colors.white,
       gray: colors.trueGray,
       indigo: colors.indigo,
       red: colors.rose,
       yellow: colors.amber,
-      green: colors.green
+      green: colors.green,
     },
     variants: {
       extend: {},
     },
-    plugins: []
+    plugins: [],
   },
   corePlugins: {
     preflight: false,
-  }
-}
->>>>>>> 0c2e7f04bf99caaa086dc90bd1147c324aca1407
+  },
+};
