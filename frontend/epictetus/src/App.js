9df4316328b73312f7008432c55f95cb9e4e571a
faboade oluwatosin <Faboadeoluwatosin@gmail.com>
2021-09-03 15:03:18 +0100

    Revert "edit sales prospect form"
    This reverts commit 1ae7e5f6f76b5a74105ff271c2a216abb85bb86d.

--- frontend/epictetus/src/App.js
+++ frontend/epictetus/src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-import Home from './components/Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import EditProspectSales from './components/EditProspectSales';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/EditProspectSales" component={EditProspectSales} />
-        </Switch>
-      </div>
-    </Router>
+    <div className="app">
+      <h1>Company Sales Prospect</h1>
+    </div>
   );
 }
 
