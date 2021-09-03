b288c07dcaa34b49f3d2f17948e9b5f74a3d872f
ASAP_A1 <aaronrickymeek@gmail.com>
2021-09-03 18:20:05 +0100

    Feature: Implemented drag and drop

--- frontend/epictetus/src/App.js
+++ frontend/epictetus/src/App.js
@@ -1,27 +1,24 @@
-import './App.css';
-import Test from './components/Test';
-import Home from './components/Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import CreateDealForm from './components/CreateDealForm';
-import EditProspectSales from './components/EditProspectSales';
+import "./App.css";
+import Test, { DragDrop } from "./components/Test";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CreateDealForm from "./components/CreateDealForm";
+import EditProspectSales from "./components/EditProspectSales";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Switch>
-                    <Route exact path="/"
-                        component={Home} />
-                    <Route exact path="/test"
-                        component={Test} />
-                    <Route exact path="/"
-                        component={CreateDealForm} />
-                    <Route exact path="/prospect"
-                        component={EditProspectSales} />
-                </Switch>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/drag-and-drop" component={DragDrop} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/test" component={Test} />
+          <Route exact path="/" component={CreateDealForm} />
+          <Route exact path="/prospect" component={EditProspectSales} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
