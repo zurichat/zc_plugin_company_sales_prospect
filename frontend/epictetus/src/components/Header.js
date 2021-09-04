961ab1f24b498a90977274ea970bb626db862c4f
Maureen Anyanwu <maureenanyanwu519@gmail.com>
2021-09-04 22:27:49 +0100

    edited onboarding fields

--- frontend/epictetus/src/components/Header.js
+++ frontend/epictetus/src/components/Header.js
@@ -1,4 +1,4 @@
-
+import '../App.css';
 import avatar from "../avatar.svg";
 export default function Header() {
   return (
@@ -8,7 +8,7 @@ export default function Header() {
       </div>
       
       <div className="search">
-        <input type="text" placeholder="search ZURI"  />
+        <input type="text" placeholder="Search ZURI" className="w-72 h-8 px-3 rounded-sm"/>
        
 
         </div>
