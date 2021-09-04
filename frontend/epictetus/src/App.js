c4a668f719b8f4cd1e96e94013698fa2a8f2809e
ASAP_A1 <aaronrickymeek@gmail.com>
2021-09-04 11:54:20 +0100

    Implementation Of Drag and Drop

--- frontend/epictetus/src/App.js
+++ frontend/epictetus/src/App.js
@@ -1,22 +1,23 @@
 import './App.css';
-import Test from './components/Test';
+import Test, { DragDrop } from './components/Test';
 import Home from './components/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import CreateDealForm from './components/CreateDealForm';
-import EditProspectSales from './components/EditProspectSales';
+
 import Prospects from './containers/Prospects';
 
 function App() {
     return (
-        <Router>
-            <div className="App font-lato">{/* lato font class added */}
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/prospects" component={Prospects} />
-                    <Route exact path="/test" component={Test} />
-                </Switch>
-            </div>
-        </Router>
+      <Router>
+        <div className="App font-lato">
+          {/* lato font class added */}
+          <Switch>
+            <Route exact path="/drag-and-drop" component={DragDrop} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/prospects" component={Prospects} />
+            <Route exact path="/test" component={Test} />
+          </Switch>
+        </div>
+      </Router>
     );
 }
 
