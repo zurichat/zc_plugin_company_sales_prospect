f507cbef59ea2ed401fbc373a185427f27c05b7f
jamil <jamilujibrilla01@gmail.com>
2021-09-04 03:26:37 +0000

    Created header component

--- frontend/epictetus/src/App.js
+++ frontend/epictetus/src/App.js
@@ -1,25 +1,27 @@
-import './App.css';
-import Test from './components/Test';
-import Home from './components/Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import CreateDealForm from './components/CreateDealForm';
-import EditProspectSales from './components/EditProspectSales';
-import Prospects from './containers/Prospects';
+import "./App.css";
+import Test from "./components/Test";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CreateDealForm from "./components/CreateDealForm";
+import EditProspectSales from "./components/EditProspectSales";
+import Prospects from "./containers/Prospects";
+import Header from "./components/Header";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/prospects" component={Prospects} />
-                    <Route exact path="/test" component={Test} />
-                    <Route exact path="/" component={CreateDealForm} />
-                    <Route exact path="/prospect" component={EditProspectSales} />
-                </Switch>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/prospects" component={Prospects} />
+          <Route exact path="/test" component={Test} />
+          <Route exact path="/" component={CreateDealForm} />
+          <Route exact path="/prospect" component={EditProspectSales} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
