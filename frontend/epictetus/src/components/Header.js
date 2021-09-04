c4a0a4dd7ef549ce207d34e682287d38953340af
Khadeeejah <wuraola81@gmail.com>
2021-09-04 16:35:22 +0200

    styled header

--- frontend/epictetus/src/components/Header.js
+++ frontend/epictetus/src/components/Header.js
@@ -1,20 +1,23 @@
-import SearchIcon from "./svg/SearchIcon";
-import SettingsIcon from "./svg/SettingsIcon";
+
 import avatar from "../avatar.svg";
 export default function Header() {
   return (
-    <header className="">
+    <header className=" header">
       <div className="">
         <h1>Sales Prospect</h1>
       </div>
-      <div className="">
-        <input type="text" placeholder="search ZURI" />
-        <SearchIcon />
-        <SettingsIcon />
+      
+      <div className="search">
+        <input type="text" placeholder="search ZURI"  />
+       
+
+        </div>
+
         <div className="avatar">
           <img src={avatar} alt="avatar" />
         </div>
-      </div>
+    
+
     </header>
   );
 }
