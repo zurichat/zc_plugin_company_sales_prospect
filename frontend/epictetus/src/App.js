89ca0a4cb71a91cd0c3e687b33f008ae68a54932
ASAP_A1 <aaronrickymeek@gmail.com>
2021-09-04 08:55:43 +0100

    Implement Click and Drag

--- frontend/epictetus/src/App.js
+++ frontend/epictetus/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Test from './components/Test';
+import Test, { DragDrop } from './components/Test';
 import Home from './components/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CreateDealForm from './components/CreateDealForm';
@@ -8,17 +8,18 @@ import Prospects from './containers/Prospects';
 
 function App() {
     return (
-        <Router>
-            <div className="App">
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/prospects" component={Prospects} />
-                    <Route exact path="/test" component={Test} />
-                    <Route exact path="/" component={CreateDealForm} />
-                    <Route exact path="/prospect" component={EditProspectSales} />
-                </Switch>
-            </div>
-        </Router>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/drag-and-drop" component={DragDrop} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/prospects" component={Prospects} />
+            <Route exact path="/test" component={Test} />
+            <Route exact path="/" component={CreateDealForm} />
+            <Route exact path="/prospect" component={EditProspectSales} />
+          </Switch>
+        </div>
+      </Router>
     );
 }
 
