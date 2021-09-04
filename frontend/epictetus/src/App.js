8b0b414ce71428ec15009ff0acf9c29dc1334b47
Abdulrahman Mohammed <abdmuhammad610@gmail.com>
2021-09-04 05:56:56 +0000

    level

--- frontend/epictetus/src/App.js
+++ frontend/epictetus/src/App.js
@@ -4,20 +4,18 @@ import Home from './components/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CreateDealForm from './components/CreateDealForm';
 import EditProspectSales from './components/EditProspectSales';
+import Prospects from './containers/Prospects';
 
 function App() {
     return (
         <Router>
             <div className="App">
                 <Switch>
-                    <Route exact path="/"
-                        component={Home} />
-                    <Route exact path="/test"
-                        component={Test} />
-                    <Route exact path="/"
-                        component={CreateDealForm} />
-                    <Route exact path="/prospect"
-                        component={EditProspectSales} />
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/prospects" component={Prospects} />
+                    <Route exact path="/test" component={Test} />
+                    <Route exact path="/" component={CreateDealForm} />
+                    <Route exact path="/prospect" component={EditProspectSales} />
                 </Switch>
             </div>
         </Router>
