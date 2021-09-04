b3cc7a47e37be509f3f62ceabb4989a63e09cf01
phaevourkhris <kristyeva93@gmail.com>
2021-09-04 10:33:01 +0100

    Create deal card  component

--- frontend/epictetus/src/App.js
+++ frontend/epictetus/src/App.js
@@ -1,23 +1,38 @@
-import './App.css';
-import Test from './components/Test';
-import Home from './components/Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import CreateDealForm from './components/CreateDealForm';
-import EditProspectSales from './components/EditProspectSales';
-import Prospects from './containers/Prospects';
+import "./App.css";
+import Test from "./components/Test";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CreateDealForm from "./components/CreateDealForm";
+import EditProspectSales from "./components/EditProspectSales";
+import Prospects from "./containers/Prospects";
+import DealCard from "./components/svg/DealCard/DealCard";
 
 function App() {
-    return (
-        <Router>
-            <div className="App font-lato">{/* lato font class added */}
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/prospects" component={Prospects} />
-                    <Route exact path="/test" component={Test} />
-                </Switch>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App font-lato">
+        {/* lato font class added */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/prospects" component={Prospects} />
+          <Route exact path="/test" component={Test} />
+          <Route
+            exact
+            path="/deals"
+            render={(props) => (
+              <DealCard
+                dealName="Jane's deal"
+                companyName="NNPC"
+                dealWorth="500,000"
+                customerEmail="janecooper@nnpc.com"
+                customerFullName="Jane Cooper"
+              />
+            )}
+          />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
