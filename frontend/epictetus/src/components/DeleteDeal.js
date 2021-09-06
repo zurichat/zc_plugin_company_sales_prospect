9415d583b0758b4acd4d546f279a85f022bf8d8a
Elijah Soladoye <shodown96@gmail.com>
2021-09-06 09:35:04 +0100

    ish fix

--- frontend/epictetus/src/components/DeleteDeal.js
+++ frontend/epictetus/src/components/DeleteDeal.js
@@ -1,34 +1,34 @@
-import React from 'react'
-import { confirmAlert } from 'react-confirm-alert'; // Import
-import DeleteDealForm from './DeleteDealForm';
+// import React from 'react'
+// import { confirmAlert } from 'react-confirm-alert'; // Import
+// import DeleteDealForm from './DeleteDealForm';
 
-// function DeleteDeal() {
+// // function DeleteDeal() {
 
-//   const submit = () => {
+// //   const submit = () => {
 
-    confirmAlert({
-      title: 'Delete  Deal',
-      message: 'This Deal will be Deleted. this action cannot be undone.',
-      buttons: [
-        {
-          label: 'No, keep ',
-          onClick: () => alert('Click No, keep')
-        },
-        {
-          label: 'Yes, Delete',
-          onClick: () => alert('Click Yes, Delete')
-        }
-      ]
-    });
-  }
+//     confirmAlert({
+//       title: 'Delete  Deal',
+//       message: 'This Deal will be Deleted. this action cannot be undone.',
+//       buttons: [
+//         {
+//           label: 'No, keep ',
+//           onClick: () => alert('Click No, keep')
+//         },
+//         {
+//           label: 'Yes, Delete',
+//           onClick: () => alert('Click Yes, Delete')
+//         }
+//       ]
+//     });
+//   }
 
-  return (
-      <div className='container'>
-        <button onClick={submit}>Delete Deal</button>
-        <DeleteDealForm/>
-      </div>
-  );
+//   return (
+//       <div className='container'>
+//         <button onClick={submit}>Delete Deal</button>
+//         <DeleteDealForm/>
+//       </div>
+//   );
 
-// }
+// // }
 
-// export default DeleteDeal; 
+// // export default DeleteDeal; 
