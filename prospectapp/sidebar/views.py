3b09fa32f4fbd7591a611fda2043228ec270f8ba
josephchinedu <josephchinedu34@gmail.com>
2021-09-14 10:46:56 +0100

    sidebar modification

--- prospectapp/sidebar/views.py
+++ prospectapp/sidebar/views.py
@@ -11,66 +11,68 @@ import json
 
 @api_view(['GET'])
 def sidebar(request):
-    if request.method == 'GET':
-        if request.GET.get('token') and request.GET.get('user') and request.GET.get('org')  :
+    if request.GET.get('token') and request.GET.get('user') and request.GET.get('org')  :
 
-            token = request.GET.get('token')
-            user_id = request.GET.get('user')
-            org = request.GET.get('user')
+        token = request.GET.get('token')
+        user_id = request.GET.get('user')
+        org = request.GET.get('user')
 
-            
+        
 
-            headers = {
-                'Authorization': f'Bearer {token}',
+        headers = {
+            'Authorization': f'Bearer {token}',
+        }
+
+        response = requests.get(f'https://api.zuri.chat/users/{user_id}', headers=headers)
+
+        res = json.loads(response.text)
+        if res['status'] == 200:
+            data = {
+                "name": settings.PLUGIN_NAME,
+                "plugin_id": settings.PLUGIN_ID,
+                "description": settings.DESCRIPTION,
+                "organisation_id": org,
+                "group_name": "Company Sales Prospects",
+                "user_id": f"{user_id}",
+                "show_group": False,
+                "joined_rooms": [],
+                "public_rooms": [
+                    {
+                        "title": "prospects",
+                        "id": "DFGfH-EDDDDS-DFDDF",
+                        "unread": 0,
+                        "members": 32,
+                        "icon" : "https://i.pinimg.com/564x/98/8e/ba/988eba187975cb426cd5226c1bd54e65.jpg",
+                        "action" : "open",
+                        "auto-join" : True
+                    },
+
+                    {
+                        "title": "deals",
+                        "url": "https://chess.zuri.chat",
+                        "unread": 0,
+                        "members": 32,
+                        "icon" : "https://i.pinimg.com/564x/98/8e/ba/988eba187975cb426cd5226c1bd54e65.jpg",
+                        "action" : "open",
+                        "auto-join" : True
+                    },
+                ]
             }
-
-            response = requests.get(f'https://api.zuri.chat/users/{user_id}', headers=headers)
-
-            res = json.loads(response.text)
-            if res['status'] == 200:
-                data = {
-                    "name": settings.PLUGIN_NAME,
-                    "plugin_id": settings.PLUGIN_ID,
-                    "description": settings.DESCRIPTION,
-                    "organisation_id": org,
-                    "group_name": "Company Sales Prospects",
-                    "user_id": f"{user_id}",
-                    "show_group": False,
-                    "joined_rooms": [],
-                    "public_rooms": [
-                        {
-                            "title": "prospects",
-                            "id": "DFGfH-EDDDDS-DFDDF",
-                            "unread": 0,
-                            "members": 32,
-                            "icon" : "https://i.pinimg.com/564x/98/8e/ba/988eba187975cb426cd5226c1bd54e65.jpg",
-                            "action" : "open",
-                            "auto-join" : True
-                        },
-
-                        {
-                            "title": "deals",
-                            "id": "DFGfH-EDDDDS-DFDDF",
-                            "unread": 0,
-                            "members": 32,
-                            "icon" : "https://i.pinimg.com/564x/98/8e/ba/988eba187975cb426cd5226c1bd54e65.jpg",
-                            "action" : "open",
-                            "auto-join" : True
-                        },
-                    ]
-                }
-                return Response(data, status=status.HTTP_200_OK)
-            else:
-                data = {
-                    "error": res['status'],
-                    "message": res['message']
-                }
-                print(res)
-                return Response(data, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data, status=status.HTTP_200_OK)
         else:
             data = {
-                "error": "400",
-                "message": "check your query paramters"
+                "error": res['status'],
+                "message": res['message']
             }
+            print(res)
             return Response(data, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        data = {
+            "error": "400",
+            "message": "check your query paramters"
+        }
+        return Response(data, status=status.HTTP_400_BAD_REQUEST)
         
+        
+
+
