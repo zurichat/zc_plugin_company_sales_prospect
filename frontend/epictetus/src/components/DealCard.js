9e47118eb430995eb7037f144b3672fab0b8954a
ASAP_A1 <aaronrickymeek@gmail.com>
2021-09-18 19:27:50 +0100

    deal category edits

--- frontend/epictetus/src/components/DealCard.js
+++ frontend/epictetus/src/components/DealCard.js
@@ -1,31 +1,95 @@
-import React from 'react'
+import React from "react";
 import avatar from "../avatar.svg";
 import { FileText, MoreVertical } from "react-feather";
 
-function DealCard({ key, draggableId, index }) {
-    return (
-        <div className="h-36 my-2 mx-4 text-left text-sm border cursor-pointer border-gray-400 p-2">
-            <div className="flex justify-between items-center">
+const DealCard = ({
+  data,
+  name,
+  company,
+  dealStage,
+  amount,
+  key,
+  draggableId,
+  index,
+}) => {
+  const items = [
+    {
+      id: 1,
+      name: "",
+      company: "",
+      amount: "",
+      email: "",
+      category: "prospect",
+    },
+    {
+      id: 2,
+      name: "",
+      company: "",
+      amount: "",
+      email: "",
+      category: "proposal",
+    },
+    {
+      id: 3,
+      name: "",
+      email: "",
+      company: "",
+      amount: "",
+      category: "negotiation",
+    },
+    {
+      id: 4,
+      name: "",
+      email: "",
+      company: "",
+      amount: "",
+      category: "closed",
+    },
+    {
+      id: 5,
+      name: "",
+      email: "",
+      company: "",
+      amount: "",
+      category: "closed",
+    },
+  ];
+   
+
+  return (
+    <>
+      {items.map((item) => {
+        const { id, name, company, amount, email, category } = item;
+
+        
+          return (
+            <div
+              key={id}
+              className="h-36 my-2 mx-4 text-left text-sm border cursor-pointer border-gray-300 rounded p-2"
+            >
+              <div className="flex justify-between items-center">
                 <div className="flex">
-                    <FileText className="w-8 mr-4" strokeWidth={1} />
-                    <span className="font-bold text-lg">Frank's deal </span>
+                  <FileText className="w-8 mr-4" strokeWidth={1} />
+                  <span className="font-bold text-lg">{name} deal </span>
                 </div>
                 <MoreVertical className="text-gray-600" strokeWidth={1} />
-            </div>
-            <div className="flex items-end">
+              </div>
+              <div className="flex items-end">
                 <img src={avatar} alt="avatar" className="w-8 mr-4" />
                 <span className="mt-2">
-                    <p>NNPC</p>
-                    <p className="mb-2">$ 500, 000</p>
-                    <p>frank.dnar@nnpc.com</p>
-                    <p>Frank Dnar</p>
+                  <p>{company}</p>
+                  <p className="mb-2">${amount}</p>
+                  <p>{email} </p>
+                  <p>{name}</p>
                 </span>
+              </div>
             </div>
-        </div>
-    )
-}
-
-
+          );
+        
+      })}
+    </>
+  );
+};
 
 // import { Draggable } from 'react-beautiful-dnd';
 // function DealCard({ key, draggableId, index }) {
@@ -58,4 +122,4 @@ function DealCard({ key, draggableId, index }) {
 //         </Draggable>
 //     )
 // }
-export default DealCard
+export default DealCard;
--- frontend/epictetus/src/containers/Deals.js
+++ frontend/epictetus/src/containers/Deals.js
@@ -78,67 +78,139 @@ function Deals() {
                         <Input placeholder="Enter Amount" />
                     </div>
                 </div>
-
-                <div className="mt-4 flex justify-end">
-                    <button
-                        type="button"
-                        className="bg-primary text-white px-10 py-2"
-                        onClick={handleCloseModal}>
-                        Create
-                    </button>
-                </div>
-            </Modal>
-            <div className="deals-grid">
-                <div className="w-max lg:w-full">
-                    <div className="grid grid-cols-4 mt-5 bg-gray-200 border border-gray-400">
-                        <div className="px-24 lg:px-8 text-center border-r border-gray-400 py-2">
-                            <span className="block font-bold text-lg">Prospects</span>
-                            <span className="text-sm">3 deals • $ 1,500,000</span>
-                        </div>
-                        <div className="px-24 lg:px-8 text-center border-r border-gray-400 py-2">
-                            <span className="block font-bold text-lg">Proposal</span>
-                            <span className="text-sm">3 deals • $ 1,500,000</span>
-                        </div>
-                        <div className="px-24 lg:px-8 text-center border-r border-gray-400 py-2">
-                            <span className="block font-bold text-lg">Negotiation</span>
-                            <span className="text-sm">3 deals • $ 1,500,000</span>
-                        </div>
-                        <div className="px-24 lg:px-8 text-center py-2">
-                            <span className="block font-bold text-lg">Closed</span>
-                            <span className="text-sm">3 deals • $ 1,500,000</span>
-                        </div>
-                    </div>
+import React, { useState} from "react";
+// import { X } from 'react-feather'
+import Button from "../components/Button";
+import DealCard from "../components/DealCard";
+import Input from "../components/Input";
+import Modal from "../components/Modal";
+import Select from "../components/Select";
 
 
-                    <div className="grid grid-cols-4 border border-t-0 border-gray-400 h-screen2">
-                        <div className="text-center border-r border-gray-400 py-2 overflow-x-auto">
-                            <DealCard />
-                            <DealCard />
-                            <DealCard />
-                            <DealCard />
-                        </div>
-                        <div className="text-center border-r border-gray-400 py-2 overflow-x-auto">
-                            <DealCard />
-                            <DealCard />
-                        </div>
-                        <div className="text-center border-r border-gray-400 py-2 overflow-x-auto">
-                            <DealCard />
-                        </div>
-                        <div className="text-center py-2 overflow-x-auto">
-                            <DealCard />
-                            <DealCard />
-                            <DealCard />
-                            <DealCard />
-                            <DealCard />
-                            <DealCard />
-                            <DealCard />
-                        </div>
-                    </div>
+const Deals = (items) => {
+  const [open, setOpen] = useState(false);
+  const handleOpenModal = () => setOpen(true);
+  const handleCloseModal = () => setOpen(false);
+  const [name, setName] = useState("");
+  const [company, setCompany] = useState("");
+  const [amount, setAmount] = useState("");
+  const [deals, setDeals] = useState(items)
 
-                </div>
-            </div>
-        </div>
+  const creatDealHandler = (z) => {
+    z.preventDefault()
+    {deals.map((item) => {
+      const {name, company, amount, category}= item
+      if (category === data) {
+        return(
+          setName(name),
+          setCompany(company),
+          setAmount(amount)
+        )
+      }
+    })}
+    setDeals(
     )
-}
+    console.log(z)
+  };
 
-export default Deals
+
+  return (
+    <div className="p-6">
+      <Button onClick={handleOpenModal}>Create New</Button>
+      <Modal
+        title="Create a Deal"
+        description="Provide information about your deal."
+        open={open}
+        closeModal={handleCloseModal}
+      >
+        <div className="mt-2">
+          <div>
+            <label className="block">Name</label>
+            <Input
+              type="text"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+              placeholder="Enter Full Name"
+            />
+          </div>
+          <div>
+            <label className="block">Company</label>
+            <Input
+              type="text"
+              value={company}
+              onChange={(event) => setCompany(event.target.value)}
+              placeholder="Enter Company"
+            />
+          </div>
+          <div>
+            <Select title="stage" label="Deal stage">
+              <option>Select a stage</option>
+              <option>Proposal</option>
+              <option>Closed</option>
+              <option>Negotiation</option>
+              <option>Prospect</option>
+            </Select>
+          </div>
+          <div>
+            <label className="block">Amount</label>
+            <Input
+              type="number"
+              value={amount}
+              onChange={(event) => setAmount(event.target.value)}
+              placeholder="Enter Amount"
+            />
+          </div>
+        </div>
+
+        <div className="mt-4 flex justify-end">
+          <button
+            type="submit"
+            className="bg-primary text-white px-10 py-2"
+            onChange={creatDealHandler}
+          >
+            Create
+          </button>
+        </div>
+      </Modal>
+      <div className="deals-grid">
+        <div className="w-max lg:w-full">
+          <div className="grid grid-cols-4 mt-5 bg-gray-200 border border-gray-400 rounded">
+            <div className="px-24 lg:px-8 text-center border-r border-gray-300 rounded py-2">
+              <span className="block font-bold text-lg">Prospects</span>
+              <span className="text-sm">3 deals • $ 1,500,000</span>
+            </div>
+            <div className="px-24 lg:px-8 text-center border-r border-gray-300 py-2">
+              <span className="block font-bold text-lg">Proposal</span>
+              <span className="text-sm">3 deals • $ 1,500,000</span>
+            </div>
+            <div className="px-24 lg:px-8 text-center border-r border-gray-300 py-2">
+              <span className="block font-bold text-lg">Negotiation</span>
+              <span className="text-sm">3 deals • $ 1,500,000</span>
+            </div>
+            <div className="px-24 lg:px-8 text-center py-2">
+              <span className="block font-bold text-lg">Closed</span>
+              <span className="text-sm">3 deals • $ 1,500,000</span>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-4 border border-t-0 border-gray-300 rounded h-screen2">
+            <div className="text-center border-r border-gray-300 rounded py-2 overflow-x-auto">
+              <DealCard data={"prospect"} />
+            </div>
+            <div className="text-center border-r border-gray-300 rounded py-2 overflow-x-auto">
+              <DealCard data={"proposal"} />
+            </div>
+            <div className="text-center border-r border-gray-300 rounded py-2 overflow-x-auto">
+              <DealCard data={"negotiation"} />
+            </div>
+            <div className="text-center py-2 overflow-x-auto">
+              <DealCard data={"closed"} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Deals;
