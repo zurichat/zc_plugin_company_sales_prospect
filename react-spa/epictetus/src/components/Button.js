df3c698508c18cfa8e589dcc7ae7c67a941d69c6
Elijah Soladoye <shodown96@gmail.com>
2021-09-19 08:16:55 +0100

    hbcutfcj

--- react-spa/epictetus/src/components/Button.js
+++ react-spa/epictetus/src/components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Button({ children, onClick }) {
+function Button({ children, onClick, className }) {
     return (
         <div>
-            <button onClick={onClick} className="bg-primary text-white outline-none border-0 py-2 px-4 rounded-sm hover:bg-green-300">
+            <button onClick={onClick} className={"bg-primary text-white outline-none border-0 py-2 px-4 rounded-sm hover:bg-green-300 "+className}>
                 {children}
             </button>
         </div>
