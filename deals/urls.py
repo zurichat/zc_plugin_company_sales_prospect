234e3e0c2bc594a00f74abf056307ce76fc9fd2b
rainfidelis <rainnyfidelis@yahoo.com>
2021-09-19 13:45:45 +0100

    added deal filter feature

--- deals/urls.py
+++ deals/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from deals.views import DealCreateView, DealUpdateView, DealsListView, DealsDetailView, DealsDeleteView
+from deals.views import (
+  DealCreateView, DealUpdateView, 
+  DealsListView, DealsDetailView, 
+  DealsDeleteView, DealsStageListView)
 
 urlpatterns = [
   path("create/", DealCreateView.as_view()),
@@ -7,4 +10,5 @@ urlpatterns = [
   path('list/', DealsListView.as_view()),
   path('<str:id>/', DealsDetailView.as_view()),
   path("delete/<str:id>/",  DealsDeleteView.as_view()),
+  path("stages/<str:stage>/",  DealsStageListView.as_view())
 ]
