46536f1088a44192aa84fe1d45475f0da3d6c6e3
Elijah Soladoye <shodown96@gmail.com>
2021-09-19 18:54:56 +0100

    collectstatic

--- prospectapp/settings.py
+++ prospectapp/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     # os.path.join(BASE_DIR,'frontend/epictetus/build/static'),
     # os.path.join(BASE_DIR, 'static'),
