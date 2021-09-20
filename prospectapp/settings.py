d4df1633cd5c53942c36efbc9ea8f47e5fa081fc
Elijah Soladoye <shodown96@gmail.com>
2021-09-20 12:04:58 +0100

    allowed-hosts

--- prospectapp/settings.py
+++ prospectapp/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config("DEBUG", cast=bool)
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
