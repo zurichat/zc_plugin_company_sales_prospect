c833d24825980bda2e5c9a4063b9b57c1ff1e04d
noornee <kendrickbon11@gmail.com>
2021-09-20 22:44:49 +0100

    removed a character from the DealsStageListView url pattern

--- deals/urls.py
+++ deals/urls.py
@@ -6,9 +6,9 @@ from deals.views import (
 
 urlpatterns = [
   path("create/", DealCreateView.as_view()),
-  path("update/<str:id>/", DealUpdateView.as_view()),
   path('', DealsListView.as_view()),
   path('<str:id>/', DealsDetailView.as_view()),
+  path("stage/<str:stage>/",  DealsStageListView.as_view()),
+  path("update/<str:id>/", DealUpdateView.as_view()),
   path("delete/<str:id>/",  DealsDeleteView.as_view()),
-  path("stages/<str:stage>/",  DealsStageListView.as_view())
 ]
