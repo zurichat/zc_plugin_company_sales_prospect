d8d5793adce528730c1ccc67b9cf8a3164646e62
Elijah Soladoye <shodown96@gmail.com>
2021-09-20 10:18:16 +0100

    debug

--- prospectapp/settings.py
+++ prospectapp/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", cast=bool)
-
+# DEBUG = config("DEBUG", cast=bool)
+DEBUG = True
 ALLOWED_HOSTS = []
 
 
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     #local apps
     "deals",
     "prospect",
+    "onboarding",
 ]
 
 MIDDLEWARE = [
