bcfc7489e4873e5c9dce37bb1658d56e79a6b105
Elijah Soladoye <shodown96@gmail.com>
2021-09-20 18:19:31 +0100

    minh

--- react-spa/epictetus/src/components/HomeCard.js
+++ react-spa/epictetus/src/components/HomeCard.js
@@ -11,7 +11,7 @@ export default function HomeCard({ src, text, id,handleClick, register }) {
             <div className="homeCard relative border border-gray-500">
                 <img src={src} alt="job-roles" className="block mx-auto" />
                 <p className="text-sm text-center">{text}</p>
-                <Check size="20px" className="homeCardCheck absolute top-3 right-3 text-primary border-2 border-primary rounded-full"/>
+                <Check size="20px" className="homeCardCheck absolute top-3 right-3 text-green border-2 border-green rounded-full"/>
             </div>
 
         </label>
