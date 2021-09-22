0871b7e7f38d16a357222526a2035aa7555994ef
ALASHI1 <alashimuyiwa@gmail.com>
2021-09-22 16:18:53 +0100

    fixed update prospects

--- prospect/serializers.py
+++ prospect/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 
 class ProspectSerializer(serializers.Serializer):
-    _id = serializers.CharField(max_length=100)
-    name = serializers.CharField(max_length=100)
-    email = serializers.CharField(max_length=100)
+    object_id = serializers.CharField(max_length=100)
     deal_stage = serializers.CharField(max_length=100, allow_null=True)
+    email = serializers.CharField(max_length=100)
+    name = serializers.CharField(max_length=100)
     phone_number = serializers.CharField(max_length=100)
