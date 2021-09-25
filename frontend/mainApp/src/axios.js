5ab10dba7434f8645133d8a9358f326d9e9ac10d
Elijah Soladoye <shodown96@gmail.com>
2021-09-25 03:20:08 +0100

    fix

--- frontend/mainApp/src/axios.js
+++ frontend/mainApp/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const DEBUG = true
+const DEBUG = false
 let API_ENDPOINT = "https://sales.zuri.chat/api/v1"
 
 if (DEBUG) { API_ENDPOINT = "http://127.0.0.1:8200/api/v1" }
