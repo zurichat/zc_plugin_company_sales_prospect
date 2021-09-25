aebc3a738880292a0b24f52212685bc0a2d01de2
Babatunde Adenowo <mo.xodeeq@gmail.com>
2021-09-25 08:32:25 +0100

    Linked deals to prospects and changed deals entry point

--- frontend/mainApp/src/utils.js
+++ frontend/mainApp/src/utils.js
@@ -1,30 +1,30 @@
-export const capitalize = word => {
+export const capitalize = (word) => {
   const lower = word.toLowerCase();
   return word.charAt(0).toUpperCase() + lower.slice(1);
-}
+};
 
-export const formatPropsects = (prospects) => {
-  return prospects.map(p => {
-      return {
-        id: p._id,
-        name: p.name,
-        email: p.email,
-        phone: p.phone_number,
-        status: capitalize(p.deal_stage)
-      }
-  })
-}
+export const formatProspects = (prospects) => {
+  return prospects.map((p) => {
+    return {
+      id: p._id,
+      name: p.name,
+      email: p.email,
+      phone: p.phone_number,
+      status: capitalize(p.deal_stage),
+    };
+  });
+};
 
 export const doesProspectExist = (prospects, name) => {
-  return prospects.filter(x => x.name === name).length > 0
-}
+  return prospects.filter((x) => x.name === name).length > 0;
+};
 
 export const updateProspects = (prospects, id, newDetails) => {
-  let p = prospects
-  const prospect = p.find(x => x.id === id)
+  let p = prospects;
+  const prospect = p.find((x) => x.id === id);
   const i = p.indexOf(prospect);
-  if(i > 0){
-    p[i] = newDetails
+  if (i > 0) {
+    p[i] = newDetails;
   }
-  return p
-}
+  return p;
+};
