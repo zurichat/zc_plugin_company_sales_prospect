496d9f0847ddd46a139867d7d3b95c423ccc0727
ASAP_A1 <aaronrickymeek@gmail.com>
2021-09-26 13:50:16 +0100

    deals and sales routing from sidebar

--- frontend/mainApp/src/App.js
+++ frontend/mainApp/src/App.js
@@ -3,14 +3,14 @@ import Test from "./containers/Test";
 // import Home from "./containers/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Prospects from "./containers/Prospects";
-import NoProspectsFound from './containers/NoProspectsFound';
+import NoProspectsFound from "./containers/NoProspectsFound";
 import Deals from "./containers/Deals";
 import SubHeader from "./components/SubHeader";
 import MobileHeader from "./components/MobileHeader";
-import Centrifuge from 'centrifuge';
+import Centrifuge from "centrifuge";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom";
 // import { getUserInfo } from "./utils";
 import { addToRoomURL, leaveRoomURL } from "./axios";
 // import { GetUserInfo } from "@zuri/zuri-control";
@@ -18,21 +18,22 @@ import { addToRoomURL, leaveRoomURL } from "./axios";
 // import Intro from "./containers/Intro";
 
 // const centrifuge = new Centrifuge('ws://localhost:8400/connection/websocket');
-const centrifuge = new Centrifuge('wss://realtime.zuri.chat/connection/websocket');
+const centrifuge = new Centrifuge(
+  "wss://realtime.zuri.chat/connection/websocket"
+);
 // centrifuge.setToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM3MjIiLCJleHAiOjE2MzMxMDI5MTN9.Fs1kg9yvQ1WXdmAoKayDjgP7PovZ6NjWBCS8xyb4J3M");
 
 function App() {
-
   const [state, setState] = useState(null);
   const [rooms, setRooms] = useState([]);
   // const history = useHistory();
 
   useEffect(() => {
-    centrifuge.on('connect', function (ctx) {
+    centrifuge.on("connect", function (ctx) {
       console.log("CONNECTED", ctx);
     });
 
-    centrifuge.on('disconnect', function (ctx) {
+    centrifuge.on("disconnect", function (ctx) {
       console.log("DISCONNECTED", ctx);
     });
 
@@ -50,95 +51,98 @@ function App() {
 
     // Prospects listener
     centrifuge.subscribe("Prospects", function (ctx) {
-      const data = ctx.data
-      console.log(data)
+      const data = ctx.data;
+      console.log(data);
       setState(data);
       // Check if the user is the same as the logged in user
       if (data.token === "elijah") {
         if (data.event === "join") {
           const roomPayload = {
-            "user": "elijah", // logged in user
-            "room_name": "Prospects"
-          }
+            user: "elijah", // logged in user
+            room_name: "Prospects",
+          };
           // history.push("/prospects")
-          axios.post(addToRoomURL, roomPayload)
-            .then(r => {
-              console.log(r.data);
-              // setRooms([...rooms, "Prospects"])
-            })
+          axios.post(addToRoomURL, roomPayload).then((r) => {
+            console.log(r.data);
+            // setRooms([...rooms, "Prospects"])
+          });
         } else if (data.event === "leave") {
           const roomPayload = {
-            "user": "elijah", // logged in user
-            "room_name": "Prospects"
-          }
+            user: "elijah", // logged in user
+            room_name: "Prospects",
+          };
           // history.push("/onboarding")
-          axios.post(leaveRoomURL, roomPayload)
-            .then(r => {
-              console.log(r.data);
-              // const filteredRooms = rooms.filter(x !== "Prospects")
-              // setRooms(filteredRooms)
-            })
+          axios.post(leaveRoomURL, roomPayload).then((r) => {
+            console.log(r.data);
+            // const filteredRooms = rooms.filter(x !== "Prospects")
+            // setRooms(filteredRooms)
+          });
         }
       }
-
     });
 
     // Deals listener
     centrifuge.subscribe("Deals", function (ctx) {
-      const data = ctx.data
-      console.log(data)
+      const data = ctx.data;
+      console.log(data);
       setState(data);
 
       // Check if the user is the same as the logged in user
       if (data.token === "elijah") {
         if (data.event === "join") {
           const roomPayload = {
-            "user": "elijah", // logged in user
-            "room_name": "Deals"
-          }
+            user: "elijah", // logged in user
+            room_name: "Deals",
+          };
           // history.push("/sales/deals")
-          axios.post(addToRoomURL, roomPayload)
-            .then(r => {
-              console.log(r.data);
-              // setRooms([...rooms, "Deals"])
-            })
+          axios.post(addToRoomURL, roomPayload).then((r) => {
+            console.log(r.data);
+            // setRooms([...rooms, "Deals"])
+          });
         } else if (data.event === "leave") {
           const roomPayload = {
-            "user": "elijah", // logged in user
-            "room_name": "Deals"
-          }
+            user: "elijah", // logged in user
+            room_name: "Deals",
+          };
           // history.push("/sales/onboarding")
-          axios.post(leaveRoomURL, roomPayload)
-            .then(r => {
-              console.log(r.data);
-              // const filteredRooms = rooms.filter(x !== "Deals")
-              // setRooms(filteredRooms)
-            })
+          axios.post(leaveRoomURL, roomPayload).then((r) => {
+            console.log(r.data);
+            // const filteredRooms = rooms.filter(x !== "Deals")
+            // setRooms(filteredRooms)
+          });
         }
       }
-
     });
     centrifuge.connect();
-  }, [])
+  }, []);
 
   return (
-        <Router basename="/sales">
-          <div className="App font-lato" >
-            {/* <Header /> */}
-            <SubHeader/>
-            <MobileHeader/>
-            <Switch >
-              <Route exact path="/" component={Prospects} />
-              <Route exact path="/deals" component={Deals} />
-              <Route exact path="/test" component={Test} />
-              <Route exact path="/NoProspectsFound" component={Test} />
-              {/*  */}
-            </Switch>
+    <Router basename="/sales">
+      <div className="App font-lato">
+        {/* <Header /> */}
+        <SubHeader />
+        <MobileHeader />
+        <Switch>
+          <Route path="/" component={Prospects} />
+          <Route
+            exact
+            path="/sales/614f651dcf2c0f1ad7585002"
+            component={Prospects}
+          />
+          <Route path="/deals" component={Deals} />
+          <Route
+            exact
+            path="/sales/614f63b8cf2c0f1ad7584ffe"
+            component={Deals}
+          />
 
-          </div>
-        </ Router >
+          <Route exact path="/test" component={Test} />
+          <Route exact path="/NoProspectsFound" component={Test} />
+          {/*  */}
+        </Switch>
+      </div>
+    </Router>
   );
-
 }
 
-export default App
+export default App;
