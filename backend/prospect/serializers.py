74f548a41931f3af8901d63b014ffe86b88b978c
Ifihanagbara Olusheye <170407044@live.unilag.edu.ng>
2021-09-30 17:52:13 +0100

    Update serializers.py

--- backend/prospect/serializers.py
+++ backend/prospect/serializers.py
@@ -1,9 +1,9 @@
-from typing_extensions import Required
+from typing_extensions import required
 from rest_framework import serializers
 
 
 class ProspectSerializer(serializers.Serializer):
-    object_id = serializers.CharField(max_length=100, Required=False)
+    object_id = serializers.CharField(max_length=100, required=False)
     name = serializers.CharField(max_length=100)
     email = serializers.CharField(max_length=100)
     phone_number = serializers.CharField(max_length=100)
