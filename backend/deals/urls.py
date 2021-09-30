13b93fed7a5c02da124401962b6f0809311e7d55
mikhail olusanya <mikhail.olusanya@gmail.com>
2021-09-30 21:24:41 +0100

    Revert "performed tests for edgecases "

--- backend/deals/urls.py
+++ backend/deals/urls.py
@@ -11,10 +11,10 @@ from deals.views import (
 
 urlpatterns = [
     path("create/", DealCreateView.as_view()),
-    path("update/", DealUpdateView.as_view()),
+    path("update/<str:id>/", DealUpdateView.as_view()),
     path("", DealsListView.as_view()),
     path("re-arrange/", ReArrangeDeals.as_view()),
-    path("delete/", DealsDeleteView.as_view()),
+    path("delete/<str:id>/", DealsDeleteView.as_view()),
     # path("stage/", DealsStageListView.as_view()),
-    path("filter/", DealsFilterListView.as_view()),
+    path("filter/<str:search>/", DealsFilterListView.as_view()),
 ]
