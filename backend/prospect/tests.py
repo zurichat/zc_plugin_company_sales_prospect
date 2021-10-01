b179713b7edbd0f30de42a0bf3676864e4540f92
Sixtus Anyanwu <sikky606@gmail.com>
2021-10-01 08:20:03 +0100

    trying some stuff

--- backend/prospect/tests.py
+++ backend/prospect/tests.py
@@ -1,4 +1,16 @@
-from django.test import TestCase
 
-# Create your tests here.
-#this is a test
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+class Prospect():
+
+    def test_list_prospect(self):
+        url = 'https://sales.zuri.chat/api/v1/prospects/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_get_prospect(self):
+        url = "https://sales.zuri.chat/api/v1/prospects/"
+        data = {'_id': '614a4aa20cd11aeb2c93ca2c'}
+        response = self.client.get(url, data = data)
+        print(response.content)
