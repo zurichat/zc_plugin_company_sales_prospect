6cd24b800281d636c3dd0d6eb370f34918e509f5
Sixtus Anyanwu <sikky606@gmail.com>
2021-10-01 14:03:10 +0100

    added some data

--- backend/prospect/tests.py
+++ backend/prospect/tests.py
@@ -1,16 +1,44 @@
-
+from django.conf import settings
 from rest_framework import status
 from rest_framework.test import APITestCase
+from faker import Faker
 
-class Prospect():
+PLUGIN_ID = settings.PLUGIN_ID
+ORGANISATION_ID = settings.ORGANISATION_ID
+fake = Faker()
 
-    def test_list_prospect(self):
-        url = 'https://sales.zuri.chat/api/v1/prospects/'
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
-    def test_get_prospect(self):
-        url = "https://sales.zuri.chat/api/v1/prospects/"
-        data = {'_id': '614a4aa20cd11aeb2c93ca2c'}
-        response = self.client.get(url, data = data)
-        print(response.content)
+
+class ProspectTests(APITestCase):
+    def setUp(self):
+        self.data = {
+            "company": faker.company(),
+            "email": faker.email(),
+            "name": faker.name(),
+            "phone_number": faker.phone_number()
+        }
+         self.data_incorrect = {
+            "company": faker.email(),
+            "email": faker.company(),
+            "name": faker.phone_number(),
+            "phone_number": faker.email()
+        }
+         self.data_null = {
+            "company": "",
+            "email": "",
+            "name": "",
+            "phone_number": ""
+        }
+         self.data_excess = {
+            "company": fake.text(),
+            "email": fake.text(),
+            "name": fake.text(),
+            "phone_number": fake.text()
+        }
+
+    def test_prospect_create(self):
+        data = self.data
+        url = "http://127.0.0.1:8200/api/v1/propsects/create/"
+        res = self.client.post(url,data,format="json")
+        response = res.json()
+        print(response)
+        self.assertEqual(res.status_code,status.HTTP_201_CREATED)
