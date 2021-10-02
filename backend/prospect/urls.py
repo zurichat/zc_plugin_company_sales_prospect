06a60076117f23b732bba695e1d4aa940e43cb53
Leonard Uyanwanne <leoscribles@gmail.com>
2021-10-02 16:52:29 +0100

    we're gonna make it

--- backend/prospect/urls.py
+++ backend/prospect/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path("<str:org_id>/create/", ProspectsCreateView.as_view()),
     path("<str:org_id>/", ProspectsListView.as_view(), name="prospects"),
     path("welcome/", WelcomeView.as_view(), name="welcome_mail"),
-    path("update/", ProspectsUpdateView.as_view()),
+    path("<str:org>/update/", ProspectsUpdateView.as_view()),
     path("delete/<str:search>/", ProspectsDeleteView.as_view())
 ]
 
