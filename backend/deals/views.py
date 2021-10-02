81b79e171f5bd0aae69dd5cfce1903da93e36ee2
Ifihan <victoriaolusheye@gmail.com>
2021-10-02 09:15:13 +0100

    Batch delete

--- backend/deals/views.py
+++ backend/deals/views.py
@@ -348,6 +348,58 @@ class DealsFilterListView(APIView):
         )
 
 
+class DealsBatchDeleteView(APIView):
+
+    def post(self, request, **kwargs):
+        # check authentication
+        # if not isAuthorized(request):
+        #     return Response(data={"message": "Missing Cookie/token header or session expired"}, status=st.HTTP_401_UNAUTHORIZED)
+
+        # if not isValidOrganisation(ORGANISATION_ID, request):
+        #     return Response(data={"message": "Invalid/Missing organization id"}, status=st.HTTP_401_UNAUTHORIZED)
+        filterData = request.data.get('filter')
+
+        url = "https://api.zuri.chat/data/delete"
+        data = {
+            "bulk_delete": True,
+            "plugin_id": PLUGIN_ID,
+            "organization_id": ORGANISATION_ID,
+            # "object_id": [],
+            "collection_name": "deals",
+            "filter": {
+                    "prospect_id": {
+                        "$in": filterData
+                    }
+            }
+        }
+
+        response = requests.request("POST", url, data=json.dumps(data))
+        print(response.json())
+        if response.status_code == 200:
+            r = response.json()
+            if r["data"]["deleted_count"] == 0:
+                return Response(
+                    data={
+                        "message": "There is no deal with this object id you supplied"
+                    },
+                    status=st.HTTP_400_BAD_REQUEST,
+                )
+            centrifugo_post(
+                "Deals",
+                {
+                    "event": "delete_deals",
+                    "token": "elijah",
+                    "object": {
+                        "data": filterData
+                    },
+                },
+            )
+            return Response(data={"message": " Deal list deleted successful"}, status=st.HTTP_200_OK)
+        return Response(
+            data={"message": "Try again later"},
+            status=st.HTTP_400_BAD_REQUEST
+        )
+
 class DealsDeleteView(APIView):
     """
     An endpoint to delete a deal.
