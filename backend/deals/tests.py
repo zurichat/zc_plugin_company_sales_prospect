0517a397c3274939ba95948703a13f6350f922c0
ALASHI1 <alashimuyiwa@gmail.com>
2021-10-03 11:21:26 +0100

    added custom propsects list for deals

--- backend/deals/tests.py
+++ backend/deals/tests.py
@@ -69,150 +69,150 @@ class DealsTests(APITestCase):
 
 #FOR CREATE VIEW
  
-#     def test_deals_create_view_success(self):
-#         data = self.serializer_data
-#         url = "http://sales.zuri.chat/api/v1/deals/create/"
-#         res = requests.post(url,data,headers=headers)
-#         response = res.json()
-#         print(response)
-#         self.assertEqual(res.status_code,status.HTTP_201_CREATED)
+    def test_deals_create_view_success(self):
+        data = self.serializer_data
+        url = "http://sales.zuri.chat/api/v1/deals/create/"
+        res = requests.post(url,data,headers=headers)
+        response = res.json()
+        print(response)
+        self.assertEqual(res.status_code,status.HTTP_201_CREATED)
 
-#     def test_deals_create_view_null(self):
-#         data = self.serializer_data_null
-#         url = "http://sales.zuri.chat/api/v1/deals/create/"
-#         res = requests.post(url,data,headers = headers)
-#         response = res.json()
-#         self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
+    def test_deals_create_view_null(self):
+        data = self.serializer_data_null
+        url = "http://sales.zuri.chat/api/v1/deals/create/"
+        res = requests.post(url,data,headers = headers)
+        response = res.json()
+        self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
 
-#     def test_deals_create_view_incorrect(self):
-#         data = self.serializer_data_incorrect
-#         url = "http://sales.zuri.chat/api/v1/deals/create/"
-#         res = requests.post(url,data,headers = headers)
-#         response = res.json()
-#         self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
+    def test_deals_create_view_incorrect(self):
+        data = self.serializer_data_incorrect
+        url = "http://sales.zuri.chat/api/v1/deals/create/"
+        res = requests.post(url,data,headers = headers)
+        response = res.json()
+        self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
 
-#     def test_deals_create_view_excess_data(self):
-#         data = self.serializer_data_excess
-#         url = "http://sales.zuri.chat/api/v1/deals/create/"
-#         res = requests.post(url,data,headers = headers)
-#         response = res.json()
-#         self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
+    def test_deals_create_view_excess_data(self):
+        data = self.serializer_data_excess
+        url = "http://sales.zuri.chat/api/v1/deals/create/"
+        res = requests.post(url,data,headers = headers)
+        response = res.json()
+        self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
 
-#     def test_deals_create_view_bad_format(self):
-#         data = self.serializer_data_bad_format
-#         url = "http://sales.zuri.chat/api/v1/deals/create/"
-#         res = requests.post(url,data,headers = headers)
-#         response = res.json()
-#         self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
-
-
+    def test_deals_create_view_bad_format(self):
+        data = self.serializer_data_bad_format
+        url = "http://sales.zuri.chat/api/v1/deals/create/"
+        res = requests.post(url,data,headers = headers)
+        response = res.json()
+        self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
 
 
-# # FOR LIST VIEW 
-    # def test_deals_list_view(self):
-    #     url = "http://sales.zuri.chat/api/v1/deals/"
-    #     res = requests.get(url,headers=headers)
-    #     response = res.json()
-    #     self.assertEqual(res.status_code,status.HTTP_200_OK)
-    #     assert(len(response)>0) #if false throw a 404_NOT_FOUND error
 
-#     def test_a_deal_list_view(self):
-#         url = "http://sales.zuri.chat/api/v1/deals/?id=6158803cd756b1d989930018"#a current deal id
-#         res = requests.get(url,headers = headers)
-#         response = res.json()
-#         self.assertEqual(res.status_code,status.HTTP_200_OK)
-#         assert(len(response)>1) 
+
+# FOR LIST VIEW 
+    def test_deals_list_view(self):
+        url = "http://sales.zuri.chat/api/v1/deals/"
+        res = requests.get(url,headers=headers)
+        response = res.json()
+        self.assertEqual(res.status_code,status.HTTP_200_OK)
+        assert(len(response)>0) #if false throw a 404_NOT_FOUND error
+
+    def test_a_deal_list_view(self):
+        url = "http://sales.zuri.chat/api/v1/deals/?id=6158803cd756b1d989930018"#a current deal id
+        res = requests.get(url,headers = headers)
+        response = res.json()
+        self.assertEqual(res.status_code,status.HTTP_200_OK)
+        assert(len(response)>1) 
 
     
-#     def test_deals_filter_list_view(self):
-#         url = "http://sales.zuri.chat/api/v1/deals/filter/?filter=closed"
-#         res = requests.get(url,headers = headers)
-#         response = res.json()
-#         assert(len(response)>0)
+    def test_deals_filter_list_view(self):
+        url = "http://sales.zuri.chat/api/v1/deals/filter/?filter=closed"
+        res = requests.get(url,headers = headers)
+        response = res.json()
+        assert(len(response)>0)
 
-#     def test_deals_filter_list_view_none(self):
-#         url = "http://sales.zuri.chat/api/v1/deals/filter/?filter=siggghhhhh"#no deal filter item
-#         res = requests.get(url,headers = headers)
-#         response = res.json()
-#         self.assertEqual((response["data"]),None)
+    def test_deals_filter_list_view_none(self):
+        url = "http://sales.zuri.chat/api/v1/deals/filter/?filter=siggghhhhh"#no deal filter item
+        res = requests.get(url,headers = headers)
+        response = res.json()
+        self.assertEqual((response["data"]),None)
 
 
 
 # FOR UPDATE VIEW
 
-    # def test_deals_update_view(self):
-    #     data = self.serializer_data
-    #     url = "http://sales.zuri.chat/api/v1/deals/update/?id=61588af4995de0bb93b94f4e"
-    #     res = requests.put(url,data,headers = headers)
-    #     response = res.json()
-    #     print(res,response)
-    #     self.assertEqual(res.status_code,status.HTTP_200_OK)
+    def test_deals_update_view(self):
+        data = self.serializer_data
+        url = "http://sales.zuri.chat/api/v1/deals/update/?id=61588af4995de0bb93b94f4e"
+        res = requests.put(url,data,headers = headers)
+        response = res.json()
+        print(res,response)
+        self.assertEqual(res.status_code,status.HTTP_200_OK)
 
 
-#     def test_deals_update_view_incorrect(self):
-#         data = self.serializer_data
-#         url = "http://sales.zuri.chat/api/v1/deals/update/?id=wrongid"#using the wrong id
-#         res = requests.put(url,data,headers = headers)
-#         response = res.json()
-#         print(res,response)
-#         self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
+    def test_deals_update_view_incorrect(self):
+        data = self.serializer_data
+        url = "http://sales.zuri.chat/api/v1/deals/update/?id=wrongid"#using the wrong id
+        res = requests.put(url,data,headers = headers)
+        response = res.json()
+        print(res,response)
+        self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
 
-#     def test_deals_update_view_incorrect_values(self):
-#         data = self.serializer_data_incorrect
-#         url = "http://sales.zuri.chat/api/v1/deals/update/?id=61537820627d729efec46b16"
-#         res = requests.put(url,data,headers = headers)
-#         response = res.json()
-#         print(res,response)
-#         self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
+    def test_deals_update_view_incorrect_values(self):
+        data = self.serializer_data_incorrect
+        url = "http://sales.zuri.chat/api/v1/deals/update/?id=61537820627d729efec46b16"
+        res = requests.put(url,data,headers = headers)
+        response = res.json()
+        print(res,response)
+        self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
 
-#     def test_deals_update_view_excess_data(self):
-#         data = self.serializer_data_excess
-#         url = "http://sales.zuri.chat/api/v1/deals/update/?id=61537820627d729efec46b16"
-#         res = requests.put(url,data,headers = headers)
-#         response = res.json()
-#         print(res,response)
-#         self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
+    def test_deals_update_view_excess_data(self):
+        data = self.serializer_data_excess
+        url = "http://sales.zuri.chat/api/v1/deals/update/?id=61537820627d729efec46b16"
+        res = requests.put(url,data,headers = headers)
+        response = res.json()
+        print(res,response)
+        self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
 
-#     def test_deals_update_view_bad_format(self):
-#         data = self.serializer_data_bad_format
-#         url = "http://sales.zuri.chat/api/v1/deals/update/?id=61537820627d729efec46b16"
-#         res = requests.put(url,data,headers = headers)
-#         response = res.json()
-#         print(res,response)
-#         self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
+    def test_deals_update_view_bad_format(self):
+        data = self.serializer_data_bad_format
+        url = "http://sales.zuri.chat/api/v1/deals/update/?id=61537820627d729efec46b16"
+        res = requests.put(url,data,headers = headers)
+        response = res.json()
+        print(res,response)
+        self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
 
-#     def test_deals_update_view_null(self):
-#         data = self.serializer_data_null
-#         url = "http://sales.zuri.chat/api/v1/deals/update/?id=61537820627d729efec46b16"
-#         res = requests.put(url,data,headers = headers)
-#         response = res.json()
-#         print(res,response)
-#         self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
+    def test_deals_update_view_null(self):
+        data = self.serializer_data_null
+        url = "http://sales.zuri.chat/api/v1/deals/update/?id=61537820627d729efec46b16"
+        res = requests.put(url,data,headers = headers)
+        response = res.json()
+        print(res,response)
+        self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
         
-#     def test_deals_rearrange_update_view(self):
-#         data = self.serializer_data_rearrange
-#         url = "http://sales.zuri.chat/api/v1/deals/re-arrange/?id=61588af4995de0bb93b94f4e"
-#         res = requests.put(url,data,headers = headers)
-#         response = res.json()
-#         print(res,response)
-#         self.assertEqual(res.status_code,status.HTTP_200_OK)
+    def test_deals_rearrange_update_view(self):
+        data = self.serializer_data_rearrange
+        url = "http://sales.zuri.chat/api/v1/deals/re-arrange/?id=61588af4995de0bb93b94f4e"
+        res = requests.put(url,data,headers = headers)
+        response = res.json()
+        print(res,response)
+        self.assertEqual(res.status_code,status.HTTP_200_OK)
 
 
 
 
-# #FOR DELETE VIEW
+#FOR DELETE VIEW
 
-#     def test_deals_delete_view_success(self):
-#         url = "http://sales.zuri.chat/api/v1/deals/delete/?id=615830eb87540d8d01ffc6c5"  #EXTRA DEALS TO DELETE #615830eb87540d8d01ffc6c5  #61588919995de0bb93b94ed6,61588948995de0bb93b94eda,61588966995de0bb93b94edf,61588987995de0bb93b94ee1
-#         res = requests.post(url,headers = headers)
-#         response = res.json()
-#         print(res,response)
-#         self.assertEqual(res.status_code,status.HTTP_200_OK)
+    def test_deals_delete_view_success(self):
+        url = "http://sales.zuri.chat/api/v1/deals/delete/?id=615830eb87540d8d01ffc6c5"  #EXTRA DEALS TO DELETE #615830eb87540d8d01ffc6c5  #61588919995de0bb93b94ed6,61588948995de0bb93b94eda,61588966995de0bb93b94edf,61588987995de0bb93b94ee1
+        res = requests.post(url,headers = headers)
+        response = res.json()
+        print(res,response)
+        self.assertEqual(res.status_code,status.HTTP_200_OK)
 
-#     def test_deals_delete_view_deleted(self):
-#         url = "http://sales.zuri.chat/api/v1/deals/delete/?id=6158594c87675da1c2017ad7" 
-#         res = requests.post(url,headers = headers)
-#         response = res.json()
-#         print(res,response)
-#         self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
+    def test_deals_delete_view_deleted(self):
+        url = "http://sales.zuri.chat/api/v1/deals/delete/?id=6158594c87675da1c2017ad7" 
+        res = requests.post(url,headers = headers)
+        response = res.json()
+        print(res,response)
+        self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
 
