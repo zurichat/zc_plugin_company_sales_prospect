6a3771a0772fa259007d9862bbd56073e9fb6980
ALASHI1 <alashimuyiwa@gmail.com>
2021-10-03 12:01:54 +0100

    added custom propsects list for deals

--- backend/prospect/views.py
+++ backend/prospect/views.py
@@ -71,15 +71,17 @@ def SearchProspects(request, search):
 class GetPropects(APIView):
      def get(self,request, *args, **kwargs):
          # check authentication
-        if not isAuthorized(request):
-            return handle_failed_request(response=None)
+        # if not isAuthorized(request):
+        #     return handle_failed_request(response=None)
 
-        if not isValidOrganisation(ORGANISATION_ID, request):
-            return handle_failed_request(response=None)
+        # if not isValidOrganisation(ORGANISATION_ID, request):
+        #     return handle_failed_request(response=None)
         url = f"https://api.zuri.chat/data/read/{PLUGIN_ID}/prospects/{ORGANISATION_ID}"
         response = requests.request("GET", url)
-        if response.status_code ==200:
-            return Response(response.json())
+        if response.status_code==200:
+            response= response.json()
+            contacts = "contacts"
+            return Response({"data":{contacts:response['data']}})
         return Response({"data":"no prospects found"})
          
 
