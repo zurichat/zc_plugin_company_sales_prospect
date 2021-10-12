3175beb52b6f9de660a7eefec57232c611c06a2f
Mikhail Olusanya <mikhail.olusanya@gmail.com>
2021-10-12 14:41:30 +0100

    debug

--- backend/common/views.py
+++ backend/common/views.py
@@ -323,24 +323,23 @@ class RemoveUserFromRoom(APIView):
 #     response['Access-Control-Allow-Origin'] = '*'
 #     return response
 def access_endoints(request):
-    print('eeede')
     
-    ENDPOINTS = [
-        "https://sales.zuri.chat/api/v1/prospects/",
-        "https://sales.zuri.chat/api/v1/prospects/{id}/",
-        "https://sales.zuri.chat/api/v1/prospects/create/",
-        "https://sales.zuri.chat/api/v1/prospects/update/{id}",
-        "https://sales.zuri.chat/api/v1/prospects/delete/{id}",
-        "https://sales.zuri.chat/api/v1/deals/",
-        "https://sales.zuri.chat/api/v1/deals/{id}",
-        "https://sales.zuri.chat/api/v1/deals/create/",
-        "https://sales.zuri.chat/api/v1/deals/update/{id}",
-        "https://sales.zuri.chat/api/v1/deals/delete/{id}",
-        "https://sales.zuri.chat/api/v1/add-to-room/",
-        "https://sales.zuri.chat/api/v1/room/",
-        "https://sales.zuri.chat/api/v1/leave-room/",
-        "https://sales.zuri.chat/api/v1/sidebar/",
-    ]
+    # ENDPOINTS = [
+    #     "https://sales.zuri.chat/api/v1/prospects/",
+    #     "https://sales.zuri.chat/api/v1/prospects/{id}/",
+    #     "https://sales.zuri.chat/api/v1/prospects/create/",
+    #     "https://sales.zuri.chat/api/v1/prospects/update/{id}",
+    #     "https://sales.zuri.chat/api/v1/prospects/delete/{id}",
+    #     "https://sales.zuri.chat/api/v1/deals/",
+    #     "https://sales.zuri.chat/api/v1/deals/{id}",
+    #     "https://sales.zuri.chat/api/v1/deals/create/",
+    #     "https://sales.zuri.chat/api/v1/deals/update/{id}",
+    #     "https://sales.zuri.chat/api/v1/deals/delete/{id}",
+    #     "https://sales.zuri.chat/api/v1/add-to-room/",
+    #     "https://sales.zuri.chat/api/v1/room/",
+    #     "https://sales.zuri.chat/api/v1/leave-room/",
+    #     "https://sales.zuri.chat/api/v1/sidebar/",
+    # ]
     GET_ENDPOINTS = [
         "https://sales.zuri.chat/api/v1/prospects/",
         "https://sales.zuri.chat/api/v1/prospects/{id}/",
@@ -372,7 +371,6 @@ def access_endoints(request):
         # r = requests.request("GET", url=endpoint, headers=headers)
         r = requests.get(endpoint)
         status_codes.append(r.status_code)
-        print(r.status_code)
 
         try:
             data = r.json()
@@ -385,28 +383,28 @@ def access_endoints(request):
             data = {"success": False}
         responses.append(data)
 
-    for endpoint in POST_ENDPOINTS:
-        # organisation_content = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb29raWUiOiJNVFl6TkRBeU5qazNOSHhIZDNkQlIwUlplRTVxVlRCT1YxSnNXVEpGTlUxSFZUVk5iVTVvV1ZSU2FVMVhSbXRPZHowOWZMdUpuaHVOMHZodV9Tbk13T0dXRUgwQVY5RTFNTFlCUFU2RWxwZW9Hd1BwIiwiZW1haWwiOiJrb3JkdGVjaG5vbEBnbWFpbC5jb20iLCJpZCI6IjYxNjU0NWRlY2E5MGU5MmNhYTRiMWFkNyIsIm9wdGlvbnMiOnsiUGF0aCI6Ii8iLCJEb21haW4iOiIiLCJNYXhBZ2UiOjc5NDEyMjYwNTEsIlNlY3VyZSI6ZmFsc2UsIkh0dHBPbmx5IjpmYWxzZSwiU2FtZVNpdGUiOjB9LCJzZXNzaW9uX25hbWUiOiJmNjgyMmFmOTRlMjliYTExMmJlMzEwZDNhZjQ1ZDVjNyJ9.52INh852cLlAedLPWa7Dg6n9xv9pSkvi9UcEJhNFwkc"
-        # authorization_content = request.headers['Authorization']
-        # url = 'https://api.zuri.chat/auth/verify-token/'
-        # headers = {"Authorization":authorization_content}
-        # r = requests.request("GET", url=url, headers=headers)
-        r = requests.get(endpoint)
-        status_codes.append(r.status_code)
-        print(status_codes)
+    # for endpoint in POST_ENDPOINTS:
+    #     # organisation_content = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb29raWUiOiJNVFl6TkRBeU5qazNOSHhIZDNkQlIwUlplRTVxVlRCT1YxSnNXVEpGTlUxSFZUVk5iVTVvV1ZSU2FVMVhSbXRPZHowOWZMdUpuaHVOMHZodV9Tbk13T0dXRUgwQVY5RTFNTFlCUFU2RWxwZW9Hd1BwIiwiZW1haWwiOiJrb3JkdGVjaG5vbEBnbWFpbC5jb20iLCJpZCI6IjYxNjU0NWRlY2E5MGU5MmNhYTRiMWFkNyIsIm9wdGlvbnMiOnsiUGF0aCI6Ii8iLCJEb21haW4iOiIiLCJNYXhBZ2UiOjc5NDEyMjYwNTEsIlNlY3VyZSI6ZmFsc2UsIkh0dHBPbmx5IjpmYWxzZSwiU2FtZVNpdGUiOjB9LCJzZXNzaW9uX25hbWUiOiJmNjgyMmFmOTRlMjliYTExMmJlMzEwZDNhZjQ1ZDVjNyJ9.52INh852cLlAedLPWa7Dg6n9xv9pSkvi9UcEJhNFwkc"
+    #     # authorization_content = request.headers['Authorization']
+    #     # url = 'https://api.zuri.chat/auth/verify-token/'
+    #     # headers = {"Authorization":authorization_content}
+    #     # r = requests.request("GET", url=url, headers=headers)
+    #     r = requests.get(endpoint)
+    #     status_codes.append(r.status_code)
+    #     print(status_codes)
 
-        try:
-            data = r.json()
-            data["status_code"] = r.status_code
-            data["method"] = "POST"
-            data["type"] = "Status"
-            data["endpoint"] = endpoint
-            data["success"] = True
-        except:
-            data = {"success": False}
-        responses.append(data)
+    #     try:
+    #         data = r.json()
+    #         data["status_code"] = r.status_code
+    #         data["method"] = "POST"
+    #         data["type"] = "Status"
+    #         data["endpoint"] = endpoint
+    #         data["success"] = True
+    #     except:
+    #         data = {"success": False}
+    #     responses.append(data)
 
-    statuses = []
+    # statuses = []
     # report = "OK!"
     # for i in responses:
     #     statuses.append(i["success"])
