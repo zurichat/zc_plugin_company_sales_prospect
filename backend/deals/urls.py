e7ab6de667a26e8cd828b322f0b9374abb64e657
paisoncodes <iamkomolafe.o.s@gmail.com>
2021-11-13 00:35:20 +0100

    refactored deals and email_templates

--- backend/deals/urls.py
+++ backend/deals/urls.py
@@ -9,10 +9,10 @@ from deals.views import (
 from django.urls import path
 
 urlpatterns = [
-    path("org/<str:org_id>/deals/deal_id/", DealDetail.as_view()),
     path("org/<str:org_id>/deals/", Deals.as_view()),
     path("org/<str:org_id>/deals/re-arrange/", ReArrangeDeals.as_view()),
     path("org/<str:org_id>/deals/delete/batch/", DealsBatchDeleteView.as_view()),
-    # path("stage/", DealsStageListView.as_view()),
     path("org/<str:org_id>/deals/filter/", DealsFilterListView.as_view()),
+    path("org/<str:org_id>/deals/<str:object_id>/", DealDetail.as_view()),
+    # path("stage/", DealsStageListView.as_view()),
 ]
